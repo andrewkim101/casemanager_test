7ed6efba2fec8b7f9411a435e10fc28cb624f232
kronikus <andrew@houseofkim.info>
2019-11-25 22:18:05 -0500

    fix jenkins

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-.
-./main.py
-./__init__.py
-./test_login.py
+import unittest
+from test_login import LoginTest
+#from .test_ime import SearchTests
+
+# save classes as tests
+#search_tests = unittest.TestLoader().loadTestsFromTestCase(SearchTests)
+#homepage_tests = unittest.TestLoader().loadTestsFromTestCase(HomePageTests)
+log_in_test = unittest.TestLoader().loadTestsFromTestCase(LoginTest)
+# create a suite from loaded TestCases
+test_list =[log_in_test]
+regression = unittest.TestSuite(test_list)
+
+# execute the test
+unittest.TextTestRunner(verbosity=2).run(regression)
